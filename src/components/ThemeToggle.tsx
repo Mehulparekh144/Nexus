9cb2434cc0a1e4728d8aa8260cf5d2405158fd23
Mehulparekh144 <mehulparekh144@gmail.com>
2025-01-19 17:54:16 -0500

    feat: Add new pages for resumes and billing
    This commit adds new pages for resumes and billing to the application. The `resumes/page.tsx` file contains the code for the resumes page, while the `billing/page.tsx` file contains the code for the billing page. These pages are added as functional components that render the main content of each page.
    
    The commit also includes the addition of a new utility function `cn` in the `utils.ts` file. This function is used to merge class names using the `clsx` and `tailwind-merge` libraries.
    
    Furthermore, the commit adds a new layout component in the `layout.tsx` file. This component is responsible for rendering the common layout structure of the application, including the navbar and the main content.
    
    Lastly, the commit includes the addition of sign-up and sign-in pages in the `sign-up/page.tsx` and `sign-in/page.tsx` files respectively. These pages use the `@clerk/nextjs` library for authentication.
    
    Please note that this commit message does not include any meta information like issue references, tags, or author names.

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+} from "./ui/dropdown-menu";
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { Button } from "./ui/button";
+import { Moon, Sun } from "lucide-react";
+
+export default function ThemeToggle() {
+	const { theme, setTheme } = useTheme();
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger>
+				<Button variant={"ghost"} size={"icon"}>
+					<Sun className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+					<Moon className="absolute size-[1.2rem] rotate-90 scale-0 transition-all  dark:-scale-100" />
+					{/* Only available to screen readers */}
+					<span className="sr-only">Toggle Theme</span>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align="end">
+				<DropdownMenuItem
+					disabled={theme === "light"}
+					onClick={() => setTheme("light")}
+				>
+					Light
+				</DropdownMenuItem>
+				<DropdownMenuItem
+					disabled={theme === "dark"}
+					onClick={() => setTheme("dark")}
+				>
+					Dark
+				</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => setTheme("system")}>
+					System
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+}
